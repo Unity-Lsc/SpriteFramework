72cbbc54f34afb0e99127b092c8f639680883e2c
Unity-Lsc <lisichao3146@gmail.com>
2024-11-13 17:12:27 +0800

    添加工具类文件

--- Assets/Boot/Scripts/Boot.cs
+++ Assets/Boot/Scripts/Boot.cs
@@ -2,17 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 项目启动
+/// </summary>
 public class Boot : MonoBehaviour
 {
 
-    void Start()
-    {
-        
+    private void Awake() {
+        DontDestroyOnLoad(gameObject);
+        StartCoroutine(BootStartUp());
     }
 
-    void Update()
-    {
-        
+    /// <summary>
+    /// 项目启动入口
+    /// </summary>
+    IEnumerator BootStartUp() {
+
+        //检查热更新
+        yield return CheckHotUpdate();
+        //end
+
+        //框架初始化
+        yield return InitFramework();
+        //end
+
+        //进入游戏
+        //end
+
+        yield break;
+    }
+
+    /// <summary>
+    /// 检查热更新
+    /// </summary>
+    IEnumerator CheckHotUpdate() {
+        yield break;
+    }
+
+    /// <summary>
+    /// 框架初始化
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator InitFramework() {
+        yield break;
     }
 
 }
---
+++ Assets/Game/Scripts/GameEntry.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// сно╥хК©з
+/// </summary>
+public class GameEntry : MonoBehaviour
+{
+
+    
+
+}
